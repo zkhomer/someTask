de87c2f3643d1977df105e4bf9e024a59b2797bf
Artur <zkhomers@gmail.com>
2023-11-20 16:09:37 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
+import ghPages from 'vite-plugin-gh-pages';
 import vue from "@vitejs/plugin-vue";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/home/",
-  plugins: [vue()],
+  plugins: [vue(),ghPages],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/someTask/'
+        ? '/zkhomer.github.io/'
         : '/'
 }
